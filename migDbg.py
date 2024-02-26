15b1b1a9d56ec9746c5cdecf92cc358b166f3dbf
Stefano Belforte <stefano.belforte@cern.ch>
2024-02-26 16:47:56 +0100

    fix names

--- migDbg.py
+++ migDbg.py
@@ -2,9 +2,9 @@
 from dbs.apis.dbsClient import DbsApi
 #migUrl='https://cmsweb-testbed.cern.ch/dbs/int/phys03/DBSMigrate'
 #phy3Url = 'https://cmsweb-testbed.cern.ch/dbs/int/phys03/DBSReader'
-migUrl='https://cmsweb-production.cern.ch/dbs/prod/phys03/DBSMigrate'
-phy3Url = 'https://cmsweb-production.cern.ch/dbs/prod/phys03/DBSReader'
-globUrl='https://cmsweb-productio.cern.ch/dbs/prod/global/DBSReader'
+migUrl='https://cmsweb-prod.cern.ch/dbs/prod/phys03/DBSMigrate'
+phy3Url = 'https://cmsweb-prod.cern.ch/dbs/prod/phys03/DBSReader'
+globUrl='https://cmsweb-prod.cern.ch/dbs/prod/global/DBSReader'
 apiG   = DbsApi(url=globUrl)
 apiP3  = DbsApi(url=phy3Url)
 apiMig = DbsApi(url=migUrl)
@@ -15,7 +15,7 @@ dataset='/NonPrD0_pT-1p2_y-2p4_pp_13TeV_pythia8/RunIILowPUAutumn18DR-102X_upgrad
 #find blocks
 bList=[]
 for dic in apiG.listBlocks(dataset=dataset):
-    bl1.append(dic['block_name'])
+    bList.append(dic['block_name'])
 
 #submit migrations and collect their Id's
 migrIds=[]
